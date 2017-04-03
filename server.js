7fabb07a679b63425a3acaa97fdc9211c5087b47
ip0ke <david.cellnigg@gmail.com>
2017-04-03 20:41:30 +0200

    Cleaning

--- server.js
+++ server.js
@@ -102,8 +102,6 @@ var Summoner = mongoose.model('Summoner', SummonerSchema);
 
                     });
 
-                    
-
                 });
 
                 req.on('error', (e) => {
@@ -166,8 +164,6 @@ var Summoner = mongoose.model('Summoner', SummonerSchema);
                         });
                         console.log('entry pushed');
 
-                        
-
                     });
                     summoner.save(function (err) {
                         if (err) return handleError(err)
@@ -188,44 +184,6 @@ var Summoner = mongoose.model('Summoner', SummonerSchema);
         
        
 
-    });
-
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function(req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text : req.body.text,
-            done : false
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function(req, res) {
-        Todo.remove({
-            _id : req.params.todo_id
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
     });
 
 // application -------------------------------------------------------------
